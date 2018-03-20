57428ef9959ab1ce126d9b8a7f3c4307ae670e99
CirQ <cirq999@163.com>
2018-03-20 14:18:36 +0800

    fix logical error when filling blanks

--- orusher/browser.py
+++ orusher/browser.py
@@ -38,16 +38,20 @@ class OWhatBrowser(FirefoxWebDriver):
             return True
 
     def __add_to_share(self):
+        share = OWhatBrowser.share
         btn = self.find_by_css('.btn-selected').first._element
-        txt = self.find_by_css('.pub_input').last._element
-        OWhatBrowser.share['select_index'] = btn.get_attribute('selectindex')
-        OWhatBrowser.share['info_key'] = txt.get_attribute('key')
-        OWhatBrowser.share['info_value'] = txt.get_attribute('value')
+        share['select_index'] = btn.get_attribute('selectindex')
+        for txt in self.find_by_css('.pub_input'):
+            ele = txt._element
+            if ele.get_attribute('key') not in require_info:
+                share['info_key'] = ele.get_attribute('key')
+                share['info_value'] = ele.get_attribute('value')
 
     def __fill_blank(self):
-        css_selector = '.pub_input[key={}]'.format(OWhatBrowser.share['info_key'])
-        self.find_by_css(css_selector)[0].fill(OWhatBrowser.share['info_value'])
-        css_selector = '.btn-select[selectindex="{}"]'.format(OWhatBrowser.share['select_index'])
+        share = OWhatBrowser.share
+        css_selector = '.pub_input[key={}]'.format(share['info_key'])
+        self.find_by_css(css_selector)[0].fill(share['info_value'])
+        css_selector = '.btn-select[selectindex="{}"]'.format(share['select_index'])
         self.find_by_css(css_selector)[0].click()
 
     def _login(self, un, pw):
@@ -61,10 +65,9 @@ class OWhatBrowser(FirefoxWebDriver):
         url = self.host + '/shop/shopdetail.html?id=' + str(tid)
         self.visit(url)
         btnType = self.find_by_id('btnType')
-        while True:
+        while self.url == url:
             try:
                 btnType.click()
-                break
             except ElementClickInterceptedException:
                 time.sleep(self.sleeptime)
         for key, value in info.items():
@@ -72,9 +75,10 @@ class OWhatBrowser(FirefoxWebDriver):
             self.find_by_css(css_selector)[0].fill(value)
 
     def _buy(self):
-        while OWhatBrowser.share.empty() and self.__not_selected():
+        share = OWhatBrowser.share
+        while share.empty() and self.__not_selected():
             time.sleep(self.sleeptime)
-        if OWhatBrowser.share.empty():
+        if share.empty():
             self.__add_to_share()
         else:
             self.__fill_blank()
